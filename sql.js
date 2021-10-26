a5f419b95cfeb8376f0dfe808e04d7dfa2d783cf
lyramer <andy.wynden@gmail.com>
2021-10-25 19:22:36 -0700

    resolving no results properly

--- sql.js
+++ sql.js
@@ -31,12 +31,10 @@ const getParcelGeom = (id) => {
   console.log('getParcelGeom')
   return new Promise(function(resolve, reject) {
     client.query('SELECT get_geoJSON($1)', [id], (error, results) => {
-      if (error) {
-        reject(error)
-      }
-      console.log("results", results)
+      if (error) reject(error)
+
       if (!results) resolve(null)
-      resolve(results.rows);
+      else resolve(results.rows);
     })
   }) 
 }
